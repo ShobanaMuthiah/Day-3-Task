ff740889cb55093e581a58d81c146af800ddab78
ShobanaMuthiah <shobanam222@gmail.com>
2024-03-25 14:45:16 -1200

    update

--- readme.md
+++ readme.md
@@ -7,3 +7,9 @@ Method 1:
 
 Method 2:
     => In the 2nd method, the JSON format objects are compared using the method 'isEqual' by importing the library 'Lodash'.
+
+Task 2:
+    Using Rest API https://restcountries.com/v3.1/all and display all the country flags in the console.
+
+Task 3:
+    Using rest API to print all countries, region, subregion
--- script1.js
+++ script1.js
@@ -44,3 +44,34 @@ else
 {
     console.log("given two object are not equal");
 }
+
+
+//Task 2
+//using Rest API https://restcountries.com/v3.1/all and display all the country flags in the console
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all');
+request.send();
+request.onload=function(){
+    var data=request.response;
+    var result=JSON.parse(data);
+    for(var i=0;i<result.length;i++)
+    {
+    console.log("flag: ",result[i].flags.png);
+    }
+}
+
+//Task 3
+//using rest API to print all countries, region, subregion
+
+var request1=new XMLHttpRequest();
+request1.open('GET','https://restcountries.com/v3.1/all');
+request1.send();
+request1.onload=function(){
+    var data1=request1.response;
+    var result1=JSON.parse(data1);
+    for(var i=0;i<result1.length;i++)
+    {
+    console.log("country: ",result1[i].name.common, "Region: ", result1[i].region,"Subregion: ",result1[i].subregion);
+    }
+}
